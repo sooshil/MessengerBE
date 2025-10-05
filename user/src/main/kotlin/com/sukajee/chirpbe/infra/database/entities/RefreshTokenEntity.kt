d867cbda13e36b9d286939f0e25cd7c2e93f4b14
sooshil <sushilkaf@gmail.com>
2025-10-05 06:50:54 -0500

    implemented refresh token entity

---
+++ user/src/main/kotlin/com/sukajee/chirpbe/infra/database/entities/RefreshTokenEntity.kt
@@ -0,0 +1,39 @@
+package com.sukajee.chirpbe.infra.database.entities
+
+import com.sukajee.chirpbe.domain.model.UserId
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import java.time.Instant
+
+@Entity
+@Table(
+	name = "refresh_tokens",
+	schema = "user_service",
+	indexes = [
+		Index(name = "idx_refresh_token_user_id", columnList = "user_id"),
+		Index(name = "idx_refresh_token_user_token", columnList = "user_id, hashed_token")
+	]
+)
+class RefreshTokenEntity(
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	var id: Long = 0,
+	
+	@Column(nullable = false)
+	var userId: UserId,
+	
+	@Column(nullable = false)
+	var expiresAt: Instant,
+	
+	@Column(nullable = false)
+	var hashedToken: String,
+	
+	@CreationTimestamp
+	var createdAt: Instant = Instant.now()
+)
---
+++ user/src/main/kotlin/com/sukajee/chirpbe/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,20 @@
+package com.sukajee.chirpbe.infra.database.repositories
+
+import com.sukajee.chirpbe.domain.model.UserId
+import com.sukajee.chirpbe.infra.database.entities.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository: JpaRepository<RefreshTokenEntity, Long> {
+
+	fun findByUserIdAndHashedToken(
+		userId: UserId,
+		hashedToken: String
+	): RefreshTokenEntity?
+	
+	fun deleteByUserIdAndHashedToken(
+		userId: UserId,
+		hashedToken: String
+	)
+	
+	fun deleteByUserId(userId: UserId): RefreshTokenEntity?
+}
