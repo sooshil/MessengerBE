e2816a42e2859d5377c1ea0d905c36f2a94616f3
sooshil <sushilkaf@gmail.com>
2025-10-19 20:45:28 -0500

    retrieve client ip address

---
+++ user/src/main/kotlin/com/sukajee/chirpbe/infra/config/NginxConfig.kt
@@ -0,0 +1,11 @@
+package com.sukajee.chirpbe.infra.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "nginx")
+data class NginxConfig(
+	var trustedIps: List<String> = emptyList(),
+	var requireProxy: Boolean = true
+)
