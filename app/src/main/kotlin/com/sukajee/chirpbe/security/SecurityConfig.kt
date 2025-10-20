9a0f44c29b9886d839e6208c3b7725f873c688a5
sooshil <sushilkaf@gmail.com>
2025-10-19 22:39:00 -0500

    updated security config

--- app/src/main/kotlin/com/sukajee/chirpbe/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/sukajee/chirpbe/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.sukajee.chirpbe.security
 
+import com.sukajee.chirpbe.api.config.JwtAuthFilter
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,12 +9,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 	
 	@Bean
-	fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+	fun filterChain(
+		httpSecurity: HttpSecurity,
+		jwtAuthFilter: JwtAuthFilter
+	): SecurityFilterChain {
 		return httpSecurity
 			.csrf { it.disable() }
 			.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -21,6 +26,8 @@ class SecurityConfig {
 				auth
 					.requestMatchers("/api/v1/auth/**")
 					.permitAll()
+					.requestMatchers("/api/v1/auth/change-password")
+					.authenticated()
 					.dispatcherTypeMatchers(
 						DispatcherType.ERROR,
 						DispatcherType.FORWARD
@@ -29,6 +36,7 @@ class SecurityConfig {
 					.anyRequest()
 					.authenticated()
 			}
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 			.exceptionHandling { configurer ->
 				configurer
 					.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
