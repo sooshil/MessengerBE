d259020535949bae19061410e60916d39334daff
sooshil <sushilkaf@gmail.com>
2025-10-25 20:45:00 -0500

    rabbitmq setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 	implementation(libs.spring.boot.starter.security)
 	implementation(libs.spring.boot.starter.data.jpa)
 	implementation(libs.spring.boot.starter.data.redis)
+	implementation(libs.spring.boot.starter.amqp)
 	runtimeOnly(libs.postgresql)
 	
 //	implementation("org.springframework.boot:spring-boot-starter-actuator")
